14edf79fe14d7efbd792680bc2c40b87af292d6b
Ingo Ruhnke <grumbel@gmail.com>
2022-08-20 13:57:47 +0200

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+
+    freetype_src.url = "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.0.tar.xz";
+    freetype_src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, tinycmmc, freetype_src }:
+    tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = freetype;
+
+          freetype = pkgs.stdenv.mkDerivation rec {
+            pname = "freetype";
+            version = "2.12.0";
+
+            src = freetype_src;
+
+            # Nix includes "---build=x86_64-unknown-linux-gnu" by
+            # default, which makes freetype fail with "native C
+            # compiler is not working"
+            configurePhase = ''
+              ./configure --prefix=$out --host=i686-w64-mingw32
+            '';
+
+            nativeBuildInputs = [
+              # FIXME: What's the difference between the two?
+              # pkgs.buildPackages.gcc
+              nixpkgs.legacyPackages.x86_64-linux.gcc
+            ];
+
+            buildInputs = [
+            ];
+
+            propagatedBuildInputs = [
+            ];
+          };
+        };
+      }
+    );
+}
